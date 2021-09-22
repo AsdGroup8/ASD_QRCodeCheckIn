4f4632ff28f6b455d68522131694b36f378bd1ee
xiao zhong <zhongxiao0711@gmail.com>
2021-09-22 21:58:14 +0800

    fix: flags

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/AsdGroup8/ASD_QRCodeCheckIn/internal/db"
 	"github.com/AsdGroup8/ASD_QRCodeCheckIn/internal/log"
 	"github.com/AsdGroup8/ASD_QRCodeCheckIn/internal/service"
+	"github.com/AsdGroup8/ASD_QRCodeCheckIn/router"
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
@@ -21,10 +22,12 @@ var engine *gin.Engine
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:     "http server",
+		Use:     "server",
+		Short:   "ASD QRCODE CHECKIN BACKEND SERVER",
 		PreRunE: Initialize,
 		Run:     Run,
 	}
+	conf.InitFlags(rootCmd)
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("root cmd execute error. %v", err)
 	}
@@ -32,11 +35,13 @@ func main() {
 
 // Initialize ...
 func Initialize(cmd *cobra.Command, _ []string) error {
-	conf.Init(cmd)
+	if err := conf.Init(cmd); err != nil {
+		return err
+	}
 	if err := log.Init(conf.LogFile, conf.LogLevel); err != nil {
 		return err
 	}
-	if err := db.Init(conf.DBConnStr); err != nil {
+	if err := db.Init(conf.DBConnStr, conf.DBName); err != nil {
 		return err
 	}
 	if err := service.Init(); err != nil {
@@ -82,4 +87,9 @@ func initEngine() {
 	engine = gin.New()
 	engine.Use(ginzap.Ginzap(log.L(), time.RFC3339, false))
 	engine.Use(ginzap.RecoveryWithZap(log.L(), true))
+	// init routers
+	r := engine.Group("/api/v1")
+	router.InitAppRouter(r)
+	router.InitCliRouter(r)
+	router.InitCustomerRouter(r)
 }
